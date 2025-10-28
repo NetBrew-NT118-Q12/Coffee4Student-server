f30850a31e531b9aa78b76fd78282d9b5a6e428f
DeDe136 <hehehe8011@gmail.com>
2025-10-28 23:07:45 +0700

    update user info

--- back-end/controllers/authControllers.js
+++ back-end/controllers/authControllers.js
@@ -5,47 +5,14 @@ exports.signup = (req, res) => {
   console.log("📩 Nhận dữ liệu từ Android:", req.body);
   const { email, phone, full_name, password } = req.body;
 
-  // Kiểm tra dữ liệu đầu vào
-  if (!email || !phone || !full_name || !password) {
-    return res.status(400).json({ success: false, message: "Thiếu dữ liệu" });
-  }
-
   // Thêm user vào DB
   User.createUser(email, phone, full_name, password, (err, result) => {
     if (err) {
       console.error(err);
       return res.status(500).json({ success: false, message: "Lỗi database" });
     }
-
-    const insertedId = result.insertId;
     console.log("✅ Thêm user thành công:", insertedId);
-
-    // Truy vấn lại user vừa được tạo
-    User.findUserById(insertedId, (err2, userResult) => {
-      if (err2) {
-        console.error(err2);
-        return res.status(500).json({ success: false, message: "Lỗi lấy dữ liệu user" });
-      }
-
-      if (userResult.length === 0) {
-        return res.status(404).json({ success: false, message: "Không tìm thấy user sau khi tạo" });
-      }
-
-      const user = userResult[0];
-
-      // ✅ Trả về thông tin user cho app
-      res.status(200).json({
-        success: true,
-        message: "Đăng ký thành công",
-        user: {
-          id: user.user_id,
-          full_name: user.full_name,
-          email: user.email,
-          phone: user.phone,
-          token: "" // bạn có thể thêm JWT nếu muốn
-        }
-      });
-    });
+    res.status(200).json({ success: true, message: "Đăng ký thành công" });
   });
 };
 
@@ -81,8 +48,20 @@ exports.login = (req, res) => {
         .json({ success: false, message: "Sai mật khẩu" });
     }
 
-    // ✅ Đăng nhập thành công
-    res.status(200).json({ success: true, message: "Đăng nhập thành công"});
+      // ✅ Trả về thông tin user cho app
+      res.status(200).json({
+        success: true,
+        message: "Đăng ký thành công",
+        user: {
+          id: user.user_id,
+          full_name: user.full_name,
+          email: user.email,
+          phone: user.phone,
+          dob: user.date_of_birth,
+          render: user.sex,
+          token: "" // bạn có thể thêm JWT nếu muốn
+        }
+      });
   };
 
   // 🔹 Xác định đăng nhập bằng email hay phone
