df7f1c5d6b104b74c507ce90723b925901414b8d
vantai13 <taidoan.13072005@gmail.com>
2025-11-06 15:41:51 +0700

    hello

--- back-end/src/models/categoryModel.js
+++ back-end/src/models/categoryModel.js
@@ -1,8 +1,8 @@
-const db = require('../config/db'); // nếu bạn đang dùng MySQL connection pool
+const db = require("../config/db"); // nếu bạn đang dùng MySQL connection pool
 
 const Category = {
   getAll: (callback) => {
-    const sql = 'SELECT category_id, name, image_url FROM Categories';
+    const sql = "SELECT category_id, name, image_url FROM categories";
     db.query(sql, callback);
   },
 };
