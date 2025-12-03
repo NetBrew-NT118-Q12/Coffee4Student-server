2055d06f4ba529f8e14547ee5835b63514ae6347
vantai13 <taidoan.13072005@gmail.com>
2025-12-03 23:00:28 +0700

    fix

--- back-end/src/routes/orderRoutes.js
+++ back-end/src/routes/orderRoutes.js
@@ -2,10 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { createOrder, cancelOrder } = require("../controllers/orderController");
 
-// ✅ Route tạo đơn hàng
 router.post("/", createOrder);
-
-// ✅ Route hủy đơn hàng
 router.put("/cancel/:id", cancelOrder);
 
 module.exports = router;
